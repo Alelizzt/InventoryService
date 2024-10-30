38e49541d79030623ad8a84f8a93a93393573e53
Alelizzt <alejandrolistc@hotmail.com>
2024-10-30 15:05:34 -0500

    Project initiation

---
+++ src/test/java/com/system/inventory/InventoryService/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.inventory.InventoryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
