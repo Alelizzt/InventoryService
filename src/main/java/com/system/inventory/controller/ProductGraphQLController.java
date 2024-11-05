b752eed1e02eae363428ad9bd2253cdbf4b8d0e1
Alelizzt <alejandrolistc@hotmail.com>
2024-11-05 14:55:00 -0500

    Add mutations to complete CRUD operations with GraphQL

--- src/main/java/com/system/inventory/controller/ProductGraphQLController.java
+++ src/main/java/com/system/inventory/controller/ProductGraphQLController.java
@@ -1,15 +1,18 @@
 package com.system.inventory.controller;
 
+import com.system.inventory.dto.ProductRequest;
 import com.system.inventory.model.Category;
 import com.system.inventory.model.Product;
 import com.system.inventory.repository.CategoryRepository;
 import com.system.inventory.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class ProductGraphQLController {
@@ -42,5 +45,37 @@ public class ProductGraphQLController {
 		return categoryRepository.findById(id).orElseThrow(
 				() -> new RuntimeException(String.format("Category %s not found ",id))
 		);
+	}
+
+
+	@MutationMapping
+	public Product saveProduct(@Argument ProductRequest productRequest) {
+		Category category = categoryRepository.findById(productRequest.getCategoryId()).orElse(null);
+		Product productDB = new Product();
+		productDB.setId(UUID.randomUUID().toString());
+		productDB.setName(productRequest.getName());
+		productDB.setPrice(productRequest.getPrice());
+		productDB.setQuantity(productRequest.getQuantity());
+		productDB.setCategory(category);
+
+		return productRepository.save(productDB);
+	}
+
+	@MutationMapping
+	public Product updateProduct(@Argument String id, @Argument ProductRequest productRequest) {
+		Category category = categoryRepository.findById(productRequest.getCategoryId()).orElse(null);
+		Product productDB = new Product();
+		productDB.setId(id);
+		productDB.setName(productRequest.getName());
+		productDB.setPrice(productRequest.getPrice());
+		productDB.setQuantity(productRequest.getQuantity());
+		productDB.setCategory(category);
+
+		return productRepository.save(productDB);
+	}
+
+	@MutationMapping
+	public void deleteProduct(@Argument String id) {
+		productRepository.deleteById(id);
 	}
 }
